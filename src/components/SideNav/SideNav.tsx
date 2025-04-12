c24ebaf6856faec702da67897433a0af087c69c5
HgGabriel <hiagogabrieloliveirapinto@gmail.com>
2025-04-11 23:40:41 -0300

    update

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -1,6 +1,6 @@
 import { Link, useLocation } from "react-router";
 import styles from "./SideNav.module.css";
-import React, { useEffect } from "react";
+import React from "react";
 
 const SideNav: React.FC = () => {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -8,7 +8,7 @@ const SideNav: React.FC = () => {
   const location = useLocation();
 
   const toggleNav = () => setIsOpen(!isOpen);
-  
+
   // Verifica se o mouse está na SideNav para dispositivos maiores
   const handleMouseEnter = () => {
     if (window.innerWidth > 768) {
@@ -32,28 +32,30 @@ const SideNav: React.FC = () => {
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      <img 
-        src={isOpen ? "uninter-logo.png" : "logo.jpg"} 
-        alt="Logo" 
+      <img
+        src={isOpen ? "uninter-logo.png" : "logo.jpg"}
+        alt="Logo"
         onClick={toggleNav}
-        style={{ cursor: 'pointer' }}
+        style={{ cursor: "pointer" }}
       />
-      
+
       <ul>
         {isMobile && (
-
-        <button className={styles.toggleButton} onClick={toggleNav}>
-          <i className={`bi bi-${isOpen ? 'arrow-left' : 'arrow-right'}`}></i>
-        </button>
+          <button className={styles.toggleButton} onClick={toggleNav}>
+            <i className={`bi bi-${isOpen ? "arrow-left" : "arrow-right"}`}></i>
+          </button>
         )}
-        
+
         {[
           { path: "/", icon: "house", label: "Home" },
           { path: "/calculator", icon: "calculator", label: "Calculator" },
           { path: "/calendar", icon: "calendar", label: "Calendário" },
-          { path: "/notes", icon: "journal-bookmark-fill", label: "Notas" }
+          { path: "/notes", icon: "journal-bookmark-fill", label: "Notas" },
         ].map((item) => (
-          <li key={item.path} className={location.pathname === item.path ? styles.active : ''}>
+          <li
+            key={item.path}
+            className={location.pathname === item.path ? styles.active : ""}
+          >
             <Link to={item.path}>
               <i className={`bi bi-${item.icon}`}></i>
               {isOpen && <p>{item.label}</p>}
